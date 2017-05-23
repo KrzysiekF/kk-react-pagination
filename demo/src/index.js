bdd9c7600611303f06e6c703ef1db7d8c5c616bf
KrzysiekF <krzysztof.furtak@gmail.com>
2017-05-23 07:33:40 +0200

    Demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import {render} from 'react-dom'
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import Pagination from '../../src'
-import reducers from './reducers';
+import Pagination from "../../src";
+import reducers from "./reducers";
 
-import Prism from 'prismjs'
-import 'prismjs/themes/prism-okaidia.css'
-import 'prismjs/components/prism-handlebars.min.js'
-import 'prismjs/components/prism-lua.min.js'
+import Normalize from "normalize.css";
+import "./style.css";
+
+import Prism from "prismjs";
+import "prismjs/themes/prism-coy.css";
 
 class Demo extends Component {
-
   renderDemoList(elementsNumber = 10) {
     let count = 0;
     let list = [];
@@ -28,12 +28,56 @@ class Demo extends Component {
   render() {
     Prism.highlightAll();
 
-    return <div>
-      <h1>kk-react-pagination demo</h1>
-      <Pagination name="test"> 
-        {this.renderDemoList()}
-      </Pagination>
-    </div>
+    return (
+    <div>
+      <div className="content">
+        <h1 className="logo">KK React Pagination</h1>
+
+        <section>
+          <h2>Installation / Download</h2>
+
+          <h3>NPM</h3>
+          <pre>
+            <code className="language-bash">
+              npm install --save kk-react-pagination
+            </code>
+          </pre>
+        </section>
+
+        <section>
+          <h2>Examples</h2>
+
+          <Pagination name="test">
+            {this.renderDemoList()}
+          </Pagination>
+        </section>
+
+        <section>
+          <h2>Options</h2>
+          <table className="table">
+            <tr>
+              <th>option</th>
+              <th>default</th>
+              <th>description</th>
+            </tr>
+            <tr>
+              <td><code>pageSize</code></td>
+              <td>5 (<code>number</code>)</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><code>startPage</code></td>
+              <td>1 (<code>number</code>)</td>
+              <td></td>
+            </tr>
+          </table>
+        </section>
+
+        <footer>
+          <a href="http://krzysztof-furtak.pl" target="_blank">Krzysztof Furtak</a> &copy; copyright 2017
+        </footer>
+      </div>
+    </div>);
   }
 }
 
@@ -41,12 +85,12 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 export const store = createStoreWithMiddleware(
   reducers,
   // eslint-disable-next-line
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 render(
-    <Provider store={store}>
-      <Demo/>
-    </Provider>,
-    document.getElementById('demo'),
-  );
+  <Provider store={store}>
+    <Demo />
+  </Provider>,
+  document.getElementById("demo")
+);
