bd6908cba6c2729da192426ee0efc77a92cd39d5
Krzysiek Furtak <krzysztof.furtak@gmail.com>
2017-08-10 23:39:18 +0200

    FIX tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,37 +1,33 @@
-// import expect from 'expect';
-// import React from 'react';
-// import { render, unmountComponentAtNode } from 'react-dom';
-// import { createStore, applyMiddleware } from 'redux';
-// import reducers from 'src/reducers';
-//
-// import Component from 'src/index';
+import expect from 'expect';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'src/reducers';
+
+import Component from 'src/index';
 
 describe('Component', () => {
   let node;
 
   beforeEach(() => {
-    // node = document.createElement('div');
+    node = document.createElement('div');
   });
 
   afterEach(() => {
-    // unmountComponentAtNode(node);
+    unmountComponentAtNode(node);
   });
 
   it('displays a welcome message', () => {
-    // const createStoreWithMiddleware = applyMiddleware()(createStore);
-    // const store = createStoreWithMiddleware(
-    //     reducers,
-    // );
-    //
-    // render(<Component store={store}>
-    //   <div>1</div>
-    //   <div>2</div>
-    // </Component>, node, () => {
-    //   console.log('node.innerHTML: ', node.innerHTML);
-    //   expect(node.innerHTML)
-    //       .toEqual(
-    //           '<div data-reactroot=""><div>1</div><div>2</div><div><button disabled="">prev</button><button class="active">1</button><button disabled="">next</button></div></div>');
-    // });
-    return true;
+    const createStoreWithMiddleware = applyMiddleware()(createStore);
+    const store = createStoreWithMiddleware(
+        reducers,
+    );
+    
+    render(<Component name="test" store={store}>
+      <div>1</div>
+      <div>2</div>
+    </Component>, node, () => {
+      expect(node.querySelector('.kk-pagination')).toExist();
+    });
   });
 });
