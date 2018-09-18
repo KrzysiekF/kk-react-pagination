e6e37905989d11ad28265c058a044d06316b7b85
Krzysztof Furtak <kfurtak@najlepszefoto.pl>
2018-09-18 08:38:06 +0200

    - build

--- es/index.js
+++ es/index.js
@@ -55,6 +55,7 @@ var Pagination = function (_Component) {
     var page = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
 
     if (this.props.pagination.data && this.props.pagination.data['page-' + page]) {
+      this.props.afterPageChange(this.props.pagination['page-' + page]);
       this.props.setPageAction(page, this.props.name);
       return;
     }
@@ -73,7 +74,9 @@ var Pagination = function (_Component) {
       _this2.props.setPageAction(response.data.page, _this2.props.name);
       _this2.props.setPagesCountAction(response.data.pagesCount, _this2.props.name);
       _this2.props.setDataAction(response.data.items, response.data.page, _this2.props.name);
+
       _this2.props.afterRequest(response);
+      _this2.props.afterPageChange(_this2.props.pagination['page-' + page]);
     }).catch(function (error) {
       _this2.setState({ pending: false });
       console.error(error);
@@ -124,7 +127,6 @@ var Pagination = function (_Component) {
       return;
     }
     this.props.setPageAction(page, this.props.name);
-    this.props.afterPageChange(this.props.pagination['page-' + page]);
   };
 
   Pagination.prototype.calculateRanges = function calculateRanges() {
